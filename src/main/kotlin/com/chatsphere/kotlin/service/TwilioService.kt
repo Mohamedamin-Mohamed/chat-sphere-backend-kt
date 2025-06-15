c520f304bcb0d9545470994240fe5e8ab6236f30
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-06-15 15:45:34 -0500

    feat: add usage of configuration property class instead of field injection

--- src/main/kotlin/com/chatsphere/kotlin/service/TwilioService.kt
+++ src/main/kotlin/com/chatsphere/kotlin/service/TwilioService.kt
@@ -1,6 +1,7 @@
 package com.chatsphere.kotlin.service
 
 import com.chatsphere.kotlin.config.TwilioConfig
+import com.chatsphere.kotlin.config.properties.TwilioProperties
 import com.chatsphere.kotlin.dto.SendCodeResultDTO
 import com.chatsphere.kotlin.dto.TwilioSendCodeDTO
 import com.chatsphere.kotlin.dto.TwilioVerifyCodeDTO
@@ -8,21 +9,17 @@ import com.chatsphere.kotlin.dto.UpdateProfileDTO
 import com.twilio.rest.lookups.v1.PhoneNumberFetcher
 import com.twilio.rest.verify.v2.service.VerificationCheck
 import com.twilio.rest.verify.v2.service.VerificationCreator
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class TwilioService(
-    @Value("\${twilio.statusPending}") private val statusPending: String,
-    @Value("\${twilio.statusApproved}") private val statusApproved: String,
-    @Value("\${twilio.verificationSID}") private val serviceID: String,
+    private val twilioProperties: TwilioProperties,
     private val twilioConfig: TwilioConfig,
     private val userService: UserService
 ) {
-    companion object {
-        val logger = LoggerFactory.getLogger(TwilioService::class.java)
-    }
+    val logger: Logger = LoggerFactory.getLogger(TwilioService::class.java)
 
     fun sendCode(sendCodeDTO: TwilioSendCodeDTO): SendCodeResultDTO {
         try {
@@ -32,8 +29,8 @@ class TwilioService(
             val number = PhoneNumberFetcher(com.twilio.type.PhoneNumber(phoneNum).toString()).fetch()
 
             if (number == null) {
-                val verification = VerificationCreator(serviceID, phoneNum, "sms").create()
-                val isPending = statusPending == verification.status
+                val verification = VerificationCreator(twilioProperties.verificationSID, phoneNum, "sms").create()
+                val isPending = twilioProperties.statusPending == verification.status
 
                 return SendCodeResultDTO(isPending, "Code sent successfully.")
             } else {
@@ -50,11 +47,11 @@ class TwilioService(
             twilioConfig.connect()
 
             val verificationCheck = VerificationCheck
-                .creator(serviceID)
+                .creator(twilioProperties.verificationSID)
                 .setTo(verifyPinDTO.phoneNumber)
                 .create()
 
-            val isApproved = statusApproved == verificationCheck.status
+            val isApproved = twilioProperties.statusApproved == verificationCheck.status
             if (isApproved) {
                 val updateProfileDto = UpdateProfileDTO()
                 updateProfileDto.phoneNumber = verifyPinDTO.phoneNumber
