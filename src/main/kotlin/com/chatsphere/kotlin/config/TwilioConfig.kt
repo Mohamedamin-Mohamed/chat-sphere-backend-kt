a2a4dc51dd740724a91b9ecc8c47d64aed7348f9
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-06-15 15:37:07 -0500

    feat: add connection to twilio

--- src/main/kotlin/com/chatsphere/kotlin/config/TwilioConfig.kt
+++ src/main/kotlin/com/chatsphere/kotlin/config/TwilioConfig.kt
@@ -1,15 +1,14 @@
 package com.chatsphere.kotlin.config
 
+import com.chatsphere.kotlin.config.properties.TwilioProperties
 import com.twilio.Twilio
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class TwilioConfig(
-    @Value("\${twilio.accountSID}") private val accountSID: String,
-    @Value("\${twilio.authToken}") private val authToken: String,
+    private val twilioProperties: TwilioProperties
 ) {
     fun connect() {
-        Twilio.init(accountSID, authToken)
+        Twilio.init(twilioProperties.accountSID, twilioProperties.authToken)
     }
 }
