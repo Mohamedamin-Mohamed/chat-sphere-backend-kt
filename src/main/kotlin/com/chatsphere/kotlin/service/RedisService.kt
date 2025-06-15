89e99ec084efcfeae509d1ab38871937aea97067
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2025-06-15 15:45:19 -0500

    refac: change logger instance to be a class field

--- src/main/kotlin/com/chatsphere/kotlin/service/RedisService.kt
+++ src/main/kotlin/com/chatsphere/kotlin/service/RedisService.kt
@@ -1,9 +1,9 @@
 package com.chatsphere.kotlin.service
 
 import com.chatsphere.kotlin.config.RedisConfig
+import com.chatsphere.kotlin.config.properties.RedisProperties
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import redis.clients.jedis.Jedis
 import redis.clients.jedis.params.SetParams
@@ -11,11 +11,9 @@ import redis.clients.jedis.params.SetParams
 @Service
 class RedisService(
     private val redisConfig: RedisConfig,
-    @Value("\${redis.redisVerificationKey}") val redisKey: String
+    private val redisProperties: RedisProperties
 ) {
-    companion object {
-        val logger: Logger = LoggerFactory.getLogger(RedisService::class.java)
-    }
+    val logger: Logger = LoggerFactory.getLogger(RedisService::class.java)
 
     fun addVerificationCodeToCache(email: String, code: String): Boolean {
         logger.info("Adding verification code $code for $email to cache")
@@ -23,7 +21,7 @@ class RedisService(
             val jedis: Jedis = redisConfig.connect()
             val cacheTTLSecond = 600L
             val setParams = SetParams().ex(cacheTTLSecond)
-            val response = jedis.set(redisKey + email, code, setParams)
+            val response = jedis.set(redisProperties.verificationKey + email, code, setParams)
             response.equals("OK")
         } catch (exp: Exception) {
             throw RuntimeException(exp)
@@ -33,7 +31,7 @@ class RedisService(
     fun getVerificationCodeFromCache(email: String): String? {
         try {
             val jedis = redisConfig.connect()
-            return jedis.get(redisKey + email)?.takeIf { it.isNotEmpty() }
+            return jedis.get(redisProperties.verificationKey + email)?.takeIf { it.isNotEmpty() }
         } catch (exp: Exception) {
             throw RuntimeException(exp)
         }
